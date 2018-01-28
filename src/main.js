9709cfdeb06a76b0774919bd2ad948d9707b7369
Sam Sartor <me@samsartor.com>
2018-01-28 01:01:40 -0700

    cool assets

--- src/main.js
+++ src/main.js
@@ -2,18 +2,33 @@ import 'aframe';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
+
+import floor_img from 'assets/floor.jpg';
+import sky_img from 'assets/sky.jpg';
+import head_model from 'assets/head.gltf';
+
 class VRScene extends React.Component {
   render () {
     return (
       <Scene>
-        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
-        <Entity light={{type: 'point'}}/>
-        <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
-        <Entity text={{value: 'Hello, WebVR!'}}/>
+        <a-assets>
+          <img id='ground_texture' src={floor_img}/>
+          <img id='sky_texture' src={sky_img}/>
+          <a-asset-item id='human' src={head_model}/>
+        </a-assets>
+
+        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+        <Entity gltf-model='#human' material={{color: 'red'}}/>
+        
+        <Entity primitive='a-plane' src='#ground_texture' rotation='-90 0 0' height='100' width='100'/>
+        <Entity primitive='a-light' type='ambient' color='#445451'/>
+        <Entity primitive='a-light' type='point' intensity='2' position='2 4 4'/>
+        <Entity primitive='a-sky' height='2048' radius='30' src='#sky_texture' theta-length='90' width='2048'/>
       </Scene>
     );
   }
 }
- 
-ReactDOM.render(<VRScene/>, document.querySelector('#app'));
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(<VRScene/>, document.querySelector('#scene_container'));
+}, false);
