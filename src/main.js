969798bc6be1abbe4c7997f52548fd339f1aba5b
Sam Sartor <me@samsartor.com>
2018-01-28 02:17:51 -0700

    animated human

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import 'aframe';
+import aframe_extras from 'aframe-extras'
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -6,6 +7,28 @@ import ReactDOM from 'react-dom';
 import floor_img from 'assets/floor.jpg';
 import sky_img from 'assets/sky.jpg';
 import head_model from 'assets/head.gltf';
+
+aframe_extras.registerAll()
+
+class Human extends React.Component {
+  animations = ['sidetoside', 'shouldertoshoulder', 'upanddown'];
+
+  constructor(props) {
+    super(props);
+    this.state = { animation: this.animations[0]};
+  }
+
+  render() {
+    return (
+      <Entity 
+        gltf-model='#human'
+        animation-mixer={{
+          clip: this.animation
+        }}
+      />
+    );
+  }
+}
 
 class VRScene extends React.Component {
   render () {
@@ -17,8 +40,10 @@ class VRScene extends React.Component {
           <a-asset-item id='human' src={head_model}/>
         </a-assets>
 
+        <Entity raycaster='objects: .clickable' cursor />
+
         <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
-        <Entity gltf-model='#human' material={{color: 'red'}}/>
+        <Human />
         
         <Entity primitive='a-plane' src='#ground_texture' rotation='-90 0 0' height='100' width='100'/>
         <Entity primitive='a-light' type='ambient' color='#445451'/>
